2953cc6aef253edb374b38fe7794b5cc954c60f3
pallas2812 <56209434+pallas2812@users.noreply.github.com>
2022-08-09 10:41:26 +0800

    this is demo of push  sample first and second scripts

--- cart.sh
+++ cart.sh
@@ -1,8 +1,11 @@
 
-
 echo setting NodeJS repos
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/temp/cart.log
-echo $?
+if [$? -eq 0]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+else
+  echo -e "\e[31mFAILURE\e[0m"
+fi
 
 echo installing NodeJS
 yum install nodejs -y &>>/temp/cart.log
