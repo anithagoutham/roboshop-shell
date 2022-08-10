e9aa53df8f3ee97a1357c4832a36a70c0cf61a5c
pallas2812 <56209434+pallas2812@users.noreply.github.com>
2022-08-10 14:45:19 +0800

    this is demo of push  sample first and second scripts

--- cart.sh
+++ cart.sh
@@ -1,6 +1,27 @@
-source common.sh
+statuscheck() {
+ if [ $? -eq 0]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+else
+  echo -e "\e[31mFAILURE\e[0m"
+  exit 1
+ fi
+}
+echo setting NodeJS repos
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/temp/cart.log
+  statuscheck
 
-nodejs
+  echo installing NodeJS
+  yum install nodejs -y &>>/temp/cart.log
+  statuscheck
+
+  id roboshop &>>/temp/cart.log
+  if[ $? -ne 0 ]; then
+     echo Adding Application User
+     useradd roboshop &>>/temp/cart.log
+     statuscheck
+  fi
+
+
 
 echo Downloading Application Content
 curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip" &>>/temp/cart.log
