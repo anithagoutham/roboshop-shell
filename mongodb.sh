790531d17fd8092ab5e0f68627176cc57a543f6e
pallas2812 <56209434+pallas2812@users.noreply.github.com>
2022-08-18 15:09:14 +0800

    this is demo of push  sample first and second scripts

--- mongodb.sh
+++ mongodb.sh
@@ -1,12 +1,33 @@
-curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo
-yum install -y mongodb-org
-systemctl enable mongod
-systemctl start mongod
-systemctl restart mongod
-curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
+source common.sh
 
-cd /tmp
-unzip mongodb.zip
-cd mongodb-main
-mongo < catalogue.js
-mongo < users.js
+component = mongodb
+
+echo setup yum repo
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo
+statuscheck
+
+echo install mongodb
+yum install -y mongodb-org &>>${log}
+statuscheck
+
+echo start mongodb service
+systemctl enable mongod &>>${log} systemctl start mongod &>>${log}
+statuscheck
+
+## update the listen configuration
+
+download
+
+echo " extract the schema files"
+cd /tmp && unzip -0 mongodb.zip &>>${log}
+statuscheck
+
+echo load schema
+cd mongodb-main && mongo < catalogue.js &>>${log} && mongo < users.js &>>${log}
+statuscheck
+
+
+
+
+
+
--- redis.sh
+++ redis.sh
@@ -1,4 +1,17 @@
-# curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo
-# yum install redis-6.2.7 -y
-# systemctl enable redis
-# systemctl start redis
+source common.sh
+
+common = redis
+
+
+echo setup yum repo
+curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo &>>${log}
+statuscheck
+
+echo install redis
+yum install redis-6.2.7 -y &>>${log}
+statuscheck
+
+# update listen ip
+
+systemctl enable redis &>>${log} && systemctl start redis &>>${log}
+statuscheck
